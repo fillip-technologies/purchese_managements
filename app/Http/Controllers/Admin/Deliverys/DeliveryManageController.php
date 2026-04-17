ef4a3b071cdeba04b55250d6b396a59e768640be
Fillip Dev <developer@filliptechnologies.com>
2026-04-17 11:43:40 +0530

    create controller done

---
+++ app/Http/Controllers/Admin/Deliverys/DeliveryManageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Deliverys;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DeliveryManageController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Admin/Purchese/PurchesManageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Purchese;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PurchesManageController extends Controller
+{
+    //
+}
